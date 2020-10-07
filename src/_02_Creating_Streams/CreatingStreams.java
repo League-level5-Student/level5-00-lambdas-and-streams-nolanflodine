fc6fa5e2756b2c1b77bf2484b5c789e3250e67b3
Floyo <Floyo@DESKTOP-G71OOBP.attlocal.net>
2020-10-06 19:26:53 -0700

    Idk how to do Streams

--- src/_02_Creating_Streams/CreatingStreams.java
+++ src/_02_Creating_Streams/CreatingStreams.java
@@ -4,18 +4,20 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Stack;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CreatingStreams {
 	public static void main(String[] args) {
 		//1. Create a stream out of the following collections.
 		String[] strArr = {"one", "two", "three", "four"};
-		
+				
 		ArrayList<Integer> intList = new ArrayList<Integer>();
 		intList.add(1);
 		intList.add(2);
 		intList.add(3);
 		intList.add(4);
+
 		
 		Stack<Random> randStack = new Stack<Random>();
 		randStack.push(new Random());
